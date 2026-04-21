a8d43e79693efa4618f6d88bf740fc3c478eeedf
xufan <1807719013@qq.com>
2026-04-21 10:14:36 +0800

    bug fix

--- src/main/java/com/xufan/learn/Generics/GenericeTest.java
+++ src/main/java/com/xufan/learn/Generics/GenericeTest.java
@@ -1,5 +1,8 @@
 package com.xufan.learn.Generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author XuFan
  * @create 2026-04-19-15:51
@@ -8,5 +11,17 @@ public class GenericeTest {
     public static void main(String[] args) {
         StringPrinter printer = new StringPrinter("123");
         printer.print();
+
+        Printer<String> printer1 = new Printer<>("1233");
+        printer1.print();
+
+        List<String> list = new ArrayList<>();
+        list.add("temp1");
+        list.add("temp2");
+        print(list);
+    }
+
+    private static <T> void print(T list){
+        System.out.println(list);
     }
 }
